028473c405e6f2415ffe62973e401ab364e6e858
danyathecoder <danilapoddunby26@gmail.com>
2020-10-21 18:40:00 +0300

    lab4

---
+++ Lab4/cmake-build-debug/CMakeFiles/Lab4_22.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4_22.dir/main.cpp.o"
+  "Lab4_22"
+  "Lab4_22.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4_22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
